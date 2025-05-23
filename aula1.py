7d2895469f9198a9f04ea70e44205e067c35951c
victorhndk <victorhstefano007@gmail.com>
2025-05-23 16:47:46 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,7 @@
+nome = str(input('Digite seu nome:'))
+
+idade = int(input('Digite sua idade:'))
+
+serie = str(input('Digite sua série atual:'))
+
+print('seu nome é ', nome , ', a sua idade é', idade ,' e a sua série é', serie)

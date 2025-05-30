e07c03c9ff71d770dc2c6354170c7b5629394932
victorhndk <victorhstefano007@gmail.com>
2025-05-30 16:06:31 -0300

    Create aula2login.py

---
+++ aula2login.py
@@ -0,0 +1,7 @@
+#Preciso de um local para o usuario colocar email e senha, e algo que identifique se a senha/usuário está certo ou errado
+user = str(input('Escreva o nome de usuário da sua conta, meu consagradíssimo amigo: '))
+senha = int(input('Digite a sua miraculosa e impenetrável senha: '))
+if user == 'vitinho' and senha == 1234: 
+    print('Bem-vindo novamente, jesus cristo contemporâneo!')
+else:
+  print('Senha completamente imaculada de existir neste programa ou usuário inexistente da face da terra universal.')

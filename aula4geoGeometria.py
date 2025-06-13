c6c034756e81d51ae3634df9fa4a6463505c47b2
victorhndk <victorhstefano007@gmail.com>
2025-06-13 16:27:56 -0300

    Create aula4geoGeometria.py

---
+++ aula4geoGeometria.py
@@ -0,0 +1,11 @@
+def calcular_area_circulo(raio):
+    area =  3.14 * raio ** 2
+    return area 
+
+def calcular_area_retangulo(base, altura):
+    area = base * altura
+    return area
+    
+def calcular_area_triangulo(base, altura):
+    area = (base * altura) / 2
+    return area

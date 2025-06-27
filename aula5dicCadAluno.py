36056146df7980d834d3b6c179eff718d730f8ab
victorhndk <victorhstefano007@gmail.com>
2025-06-27 15:32:50 -0300

    Create aula5dicCadAluno.py

---
+++ aula5dicCadAluno.py
@@ -0,0 +1,11 @@
+peixe = {
+    "nome" : "Carlos Eduardo",
+    "idade" : "16",
+    "turma" : "2A"
+}
+print(peixe)
+peixe["disciplina_fav"] = "Matemática"
+print(peixe)
+peixe["idade"] = "17"
+print(peixe)
+print(f"Turma: {peixe['idade']}")

b2654c0b696ee536078e361657b886969ac9fb18
victorhndk <victorhstefano007@gmail.com>
2025-06-27 14:22:45 -0300

    Create aula5dicK.py

---
+++ aula5dick.py
@@ -0,0 +1,10 @@
+produto = {
+    "nome": "Notebook Dell",
+    "preco": "4500.00",
+    "estoque": "15",
+    "cor": "Prata"
+}
+
+print(produto)
+print(f"Nome: {produto['nome']}")
+print(f"Preço: {produto['preco']}")
